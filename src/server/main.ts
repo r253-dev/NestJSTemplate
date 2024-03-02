3b743ea34e48acac16e9c7090073bfc2183be72b
r253hmdryou <r253.hmdryou@gmail.com>
2024-03-02 22:34:33 +0900

    Remove SIGINT and SIGTERM signal handlers from main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,21 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  process.on('SIGINT', async () => {
-    console.log('SIGINT signal received.');
-    // ここでデータベース接続の解除など、クリーンアップが必要な処理を行う
-    await app.close();
-    process.exit(0);
-  });
-
-  process.on('SIGTERM', async () => {
-    console.log('SIGTERM signal received.');
-    // ここでデータベース接続の解除など、クリーンアップが必要な処理を行う
-    await app.close();
-    process.exit(0);
-  });
-
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
