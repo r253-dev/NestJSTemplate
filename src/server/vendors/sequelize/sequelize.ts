c62308e469d46c65758db1d9c45b1e37897a7b9a
r253hmdryou <r253.hmdryou@gmail.com>
2024-03-03 14:16:10 +0900

    Refactor Sequelize initialization to improve readability

--- src/server/vendors/sequelize/sequelize.ts
+++ src/server/vendors/sequelize/sequelize.ts
@@ -1,9 +1,6 @@
 import { Sequelize } from 'sequelize';
 
 const socketPath = process.env.INSTANCE_CONNECTION_NAME;
-export const sequelize = new Sequelize(
-  `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DATABASE}${socketPath ? `?socketPath=${socketPath}` : ''}`,
-  {
-    logging: false,
-  },
-);
+const URI = `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DATABASE}${socketPath ? `?socketPath=${socketPath}` : ''}`;
+console.log(URI);
+export const sequelize = new Sequelize(URI, { logging: false });
