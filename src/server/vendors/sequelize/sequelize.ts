44d10a5938a13f008cbc82a8f5e6fe354f38ac97
r253hmdryou <r253.hmdryou@gmail.com>
2024-03-03 17:07:15 +0900

    Refactor Sequelize connection URI for Google Cloud SQL compatibility

--- src/server/vendors/sequelize/sequelize.ts
+++ src/server/vendors/sequelize/sequelize.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
 const socketPath = process.env.INSTANCE_CONNECTION_NAME;
-const URI = `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DATABASE}${socketPath ? `?socketPath=${socketPath}` : ''}`;
+const URI = `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${socketPath ? `/cloudsql/${socketPath}` : process.env.DB_HOST}/${process.env.DATABASE}`;
 console.log(URI);
 export const sequelize = new Sequelize(URI, { logging: false });
