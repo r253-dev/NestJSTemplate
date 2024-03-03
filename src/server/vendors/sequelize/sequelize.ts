b7bd615ede0d2b58895f60ab2aec9794f1c280b1
r253hmdryou <r253.hmdryou@gmail.com>
2024-03-03 18:31:33 +0900

    Refactor sequelize connection setup in sequelize.ts

--- src/server/vendors/sequelize/sequelize.ts
+++ src/server/vendors/sequelize/sequelize.ts
@@ -1,20 +1,10 @@
 import { Sequelize } from 'sequelize';
 
-const connectionName = process.env.INSTANCE_CONNECTION_NAME;
-const host = connectionName ? `${connectionName}` : process.env.DB_HOST!;
+const host = process.env.DB_HOST!;
 const user = process.env.DB_USER!;
 const database = process.env.DATABASE!;
 const pass = process.env.DB_PASS;
-// const URI = `mysql://${user}:${pass}@${connectionName ? `/cloudsql/${connectionName}` : host}/${database}`;
-// console.log(URI);
-// export const sequelize = new Sequelize(URI, { logging: false });
-console.log({
-  connectionName,
-  host,
-  user,
-  database,
-  pass,
-});
+
 export const sequelize = new Sequelize(database, user, pass, {
   dialect: 'mysql',
   host: host,
