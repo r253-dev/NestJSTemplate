46799a743049d406237f1bd0f1f8e11e3b14b41f
HAMADA Ryo <r253.hmdryou@gmail.com>
2024-03-03 10:58:58 +0900

    Feature/swagger (#19)
    * Added Swagger support for API documentation and testing
    
    * Add Swagger annotations to AppController methods in server
    
    * Remove unnecessary comments in swagger.ts

---
+++ src/server/libs/swagger.ts
@@ -0,0 +1,21 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import { dump } from 'js-yaml';
+
+export function hostSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Template API')
+    .setDescription('Template API')
+    .setVersion('0.1.0')
+    .addGlobalParameters({
+      name: 'X-Requested-With',
+      in: 'header',
+      required: false,
+    })
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync('./dist/swagger-spec.yaml', dump(document));
+  SwaggerModule.setup('swagger', app, document);
+}
