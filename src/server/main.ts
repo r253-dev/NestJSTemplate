8e75b480175f55876893028763752c2ce3e7ebb9
r253hmdryou <r253.hmdryou@gmail.com>
2024-03-03 10:14:56 +0900

    Added CORS support to server and updated .env file with SERVICE_SITE_URL

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [process.env.SERVICE_SITE_URL!],
+  });
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
