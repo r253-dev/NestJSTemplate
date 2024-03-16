50fe1e03007207181201e37b202eb4617dd2588f
r253hmdryou <r253.hmdryou@gmail.com>
2024-03-16 18:17:47 +0900

    Refactor Sequelize configuration in app.module.ts to use environment variables for database connection details

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -3,15 +3,16 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from 'admin/admin.module';
+import { Dialect } from 'sequelize';
 
 @Module({
   imports: [
     SequelizeModule.forRoot({
       database: process.env.DATABASE,
-      host: process.env.RDB_HOST,
-      username: process.env.RDB_USER,
-      password: process.env.RDB_PASS,
-      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      dialect: (process.env.DB_DIALECT || 'mysql') as Dialect,
       logQueryParameters: true,
     }),
     AdminModule,
