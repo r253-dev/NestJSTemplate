6a6b4e9297f8abc787ffda515a9d30022b949e4b
r253hmdryou <r253.hmdryou@gmail.com>
2024-03-16 14:10:09 +0900

    add migration and model

---
+++ src/db/migrations/20240316-001-create-administrators.ts
@@ -0,0 +1,45 @@
+import { DataTypes, QueryInterface, Sequelize } from 'sequelize';
+
+export async function up(queryInterface: QueryInterface, _s: Sequelize) {
+  await queryInterface.createTable(
+    'administrators',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.BIGINT,
+      },
+      uuid: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      password_hash: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      uniqueKeys: {
+        uniqueUuid: {
+          fields: ['uuid'],
+        },
+        uniqueEmail: {
+          fields: ['email'],
+        },
+      },
+    },
+  );
+}
+
+export async function down(queryInterface: QueryInterface, _s: Sequelize) {
+  await queryInterface.dropTable('administrators');
+}
---
+++ src/server/share/models/administrator.model.ts
@@ -0,0 +1,53 @@
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from 'sequelize';
+import { sequelize } from 'vendors/sequelize/sequelize';
+
+export class AdministratorModel extends Model<
+  InferAttributes<AdministratorModel>,
+  InferCreationAttributes<AdministratorModel>
+> {
+  declare id: CreationOptional<bigint>;
+  declare uuid: string;
+  declare email: string;
+  declare passwordHash: string | null;
+  declare createdAt: Date;
+}
+
+AdministratorModel.init(
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    uuid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: 'uniqueUuid',
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: 'uniqueEmail',
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: 'administrators',
+    timestamps: false,
+  },
+);
